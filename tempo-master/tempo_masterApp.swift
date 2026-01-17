9c86382cb1dba52af0885c11f9b72fd836b96a29
Diego Escobar <diego.e@me.com>
2026-01-16 18:55:10 -0600

    Initial Commit

---
+++ tempo-master/tempo_masterApp.swift
@@ -0,0 +1,17 @@
+//
+//  tempo_masterApp.swift
+//  tempo-master
+//
+//  Created by Diego Escobar on 1/16/26.
+//
+
+import SwiftUI
+
+@main
+struct tempo_masterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
